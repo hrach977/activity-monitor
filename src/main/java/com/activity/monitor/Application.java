18d58be4e3d24d37de3bbba3df630cf2bdfebd07
hrach977 <hrachya.yeghishyan@picsart.com>
2020-01-15 22:45:28 +0400

    checkpoint // services stuff

--- src/main/java/com/activity/monitor/Application.java
+++ src/main/java/com/activity/monitor/Application.java
@@ -1,7 +1,11 @@
 package com.activity.monitor;
 
+import com.activity.monitor.common.SysProcess;
+import com.activity.monitor.common.SysService;
 import com.activity.monitor.domain.OperatingSystem;
 import com.activity.monitor.domain.impl.MacOperatingSystem;
+
+import java.util.List;
 
 public class Application {
 
@@ -11,6 +15,14 @@ public class Application {
         System.out.println("elevated: " + os.isElevated());
         System.out.println("proc count: " + os.getProcessCount());
         System.out.println("thread count: " + os.getThreadCount());
+
+//        List<SysProcess> sysProcesses = os.getProcesses();
+//        System.out.println("sysProcesses.size: " + sysProcesses.size());
+//        sysProcesses.forEach(System.out::println);
+        System.out.println("*******");
+        List<SysService> sysServices = os.getServices();
+        System.out.println("sysServices.size: " + sysServices.size());
+        sysServices.forEach(System.out::println);
 
     }
 }
